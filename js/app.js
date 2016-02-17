9e76981c08b86ed197a90375a521247072ab4020
aberkow <berkowitz.clarinet@gmail.com>
2016-02-17 14:56:24 -0500

    Refined functions and logic - more semantic

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
+var currentGameState = {}; //just to test put it back later
 
 $(document).ready(function(){
-	var currentGameState = {};
+	
     var userNumber; //the value the user enters
     
     startGame(); //start the game on load
@@ -17,23 +18,48 @@ $(document).ready(function(){
   	});
     
     $('.new').click(function() { //click the newGame button to load a new random #
-        //debugger;
         startGame();
     });
 
+    /*---Get the user's number input and add it to the currentGameState object.---*/
+    $('#guessButton').click(function(evt) {
+        evt.preventDefault(); 
+        compareNumbers();
+    });
+    
+    $('#userGuess').change(function(evt) {
+        var newNumber = parseInt($(evt.target).val(), 10);
+        if (isNaN(newNumber)) {
+            alert('Please enter a number');
+            $(evt.target).val(currentGameState.userNumber);
+        }
+        else {
+            currentGameState.userNumber = newNumber;
+        }
+    });
+    
+    /*---function area---*/
+    
     function startGame() { //create a random number btwn 1-100
         currentGameState.secretNumber = Math.floor((Math.random() * 100) + 1);
     }
     
-    /*---Get the user's number input and add it to the currentGameState object.---*/
-    $('#guessButton').click(function(evt) {
-        evt.preventDefault();
-        var numberAsString = $('#userGuess').val();
-        currentGameState.userNumber = Number(numberAsString);
-        console.log(currentGameState);
-        debugger;
-    });
+    function compareNumbers() {
+        if (currentGameState.secretNumber == currentGameState.userNumber) {
+            $('#feedback').text('YOU WIN!!!');           
+        }
+        else {
+            var difference = Math.abs(currentGameState.secretNumber - currentGameState.userNumber);
+            if (difference > 50) {
+                $('#feedback').text('Very cold!');
+            } else if (difference <= 50 && difference > 30) {
+                $('#feedback').text('Cold');
+            } else if (difference <= 30 && difference > 10) {
+                $('#feedback').text('Hot');
+            } else {
+                $('#feedback').text('Very hot!');
+            }
+        }
+    }
     
-
-
 });
