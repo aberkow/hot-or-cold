5b687cde0a8f57fb4d9bbf2aa6ff16cfdb3608fc
aberkow <berkowitz.clarinet@gmail.com>
2016-02-18 21:38:35 -0500

    all basic functions work

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var currentGameState = {}; //just to test put it back later
 $(document).ready(function(){
 	
     var userNumber; //the value the user enters
+    var userWinState;
     
     startGame(); //start the game on load
     
@@ -25,6 +26,9 @@ $(document).ready(function(){
     $('#guessButton').click(function(evt) {
         evt.preventDefault(); 
         compareNumbers();
+        $('#count').text(counter());
+        $('.guessBox').append('<li>' + currentGameState.userNumber + '</li>');
+        
     });
     
     $('#userGuess').change(function(evt) {
@@ -40,26 +44,43 @@ $(document).ready(function(){
     
     /*---function area---*/
     
-    function startGame() { //create a random number btwn 1-100
+    function startGame() { //create a random number btwn 1-100, clear the entry box, set #userGuess to 0
         currentGameState.secretNumber = Math.floor((Math.random() * 100) + 1);
+        currentGameState.userNumber = $('#userGuess').val('');
+        currentGameState.guessNumber = 0;
     }
+    
+    function counter() {
+            currentGameState.guessNumber += 1;
+            var count = currentGameState.guessNumber.toString();
+            return count;
+        }
     
     function compareNumbers() {
         if (currentGameState.secretNumber == currentGameState.userNumber) {
-            $('#feedback').text('YOU WIN!!!');           
+            $('#feedback').text('YOU WIN!!!'); 
+            userWinState = true;
         }
         else {
             var difference = Math.abs(currentGameState.secretNumber - currentGameState.userNumber);
             if (difference > 50) {
                 $('#feedback').text('Very cold!');
+                userWinState = false;
             } else if (difference <= 50 && difference > 30) {
                 $('#feedback').text('Cold');
+                userWinState = false;
             } else if (difference <= 30 && difference > 10) {
                 $('#feedback').text('Hot');
+                userWinState = false;
             } else {
                 $('#feedback').text('Very hot!');
+                userWinState = false;
             }
         }
     }
     
 });
+
+/*
+still needs counter and difference btwn current & previous guess
+*/
