7c4854fde166274e7f51c047034c6f0982d27b12
aberkow <berkowitz.clarinet@gmail.com>
2016-02-20 22:56:16 -0500

    refining logic

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
-var currentGameState = {}; //just to test put it back later
+var currentGameState = {};
 
 $(document).ready(function(){
 	
     var userNumber; //the value the user enters
-    var userWinState;
     
     startGame(); //start the game on load
     
-	/*--- Display information modal box ---*/
+	/*--event listener area--*/
+    
+    /*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
 
@@ -24,21 +25,26 @@ $(document).ready(function(){
 
     /*---Get the user's number input and add it to the currentGameState object.---*/
     $('#guessButton').click(function(evt) {
-        evt.preventDefault(); 
-        compareNumbers();
-        $('#count').text(counter());
+        evt.preventDefault(); //stop the form from sending
+        warmerOrColder(); //compare the numbers
+        $('#count').text(counter()); //change the counter
         $('.guessBox').append('<li>' + currentGameState.userNumber + '</li>');
-        
+        //create a list of guessed numbers
+        currentGameState.guessArray.push(currentGameState.userNumber);
+        $('#userGuess').val('').focus();
     });
     
     $('#userGuess').change(function(evt) {
-        var newNumber = parseInt($(evt.target).val(), 10);
-        if (isNaN(newNumber)) {
-            alert('Please enter a number');
-            $(evt.target).val(currentGameState.userNumber);
-        }
-        else {
-            currentGameState.userNumber = newNumber;
+        var newNumber = parseInt($(evt.target).val(), 10); //change the userGuess # from string to Int
+        if (isNaN(newNumber) || (newNumber < 1 || newNumber > 100)) {
+            alert('Please enter a number between 1 and 100');
+            $(evt.target).val(''); //alert/clear if the input is not an int btwn 1 - 100
+        } else if (currentGameState.guessCount >= 2 && (currentGameState.userNumber == currentGameState.previousNumber)) {
+            alert('Please enter a new number');
+            $(evt.target).val('');//alert/clear if two guesses are the same
+        } else {
+            currentGameState.previousNumber = currentGameState.userNumber; //set the previous number
+            currentGameState.userNumber = newNumber; //assign the value of newNumber
         }
     });
     
@@ -46,41 +52,44 @@ $(document).ready(function(){
     
     function startGame() { //create a random number btwn 1-100, clear the entry box, set #userGuess to 0
         currentGameState.secretNumber = Math.floor((Math.random() * 100) + 1);
-        currentGameState.userNumber = $('#userGuess').val('');
-        currentGameState.guessNumber = 0;
+        currentGameState.userNumber = null;//
+        currentGameState.guessCount = 0;
+        currentGameState.previousNumber = null;
+        currentGameState.guessArray = [];
+        $('#userGuess').val('');
     }
     
-    function counter() {
-            currentGameState.guessNumber += 1;
-            var count = currentGameState.guessNumber.toString();
+    function counter() { //change the guess counter
+            currentGameState.guessCount += 1;
+            var count = currentGameState.guessCount.toString();
             return count;
         }
     
-    function compareNumbers() {
-        if (currentGameState.secretNumber == currentGameState.userNumber) {
-            $('#feedback').text('YOU WIN!!!'); 
-            userWinState = true;
+    function warmerOrColder() {
+        if (isNaN(currentGameState.userNumber) || (currentGameState.userNumber < 1 || currentGameState.userNumber > 100)) {
+            return alert('Please enter a number between 1 and 100.');
         }
+        
+        if (currentGameState.secretNumber == currentGameState.userNumber) {
+           $('#feedback').text('YOU WIN!!!'); 
+         }
         else {
-            var difference = Math.abs(currentGameState.secretNumber - currentGameState.userNumber);
-            if (difference > 50) {
+           var currentDifference = Math.abs(currentGameState.secretNumber - currentGameState.userNumber);
+           var previousDifference = Math.abs(currentGameState.userNumber - currentGameState.guessArray[currentGameState.guessArray.length - 1]);      
+            
+            if(currentDifference == previousDifference) {
+                alert('Please enter a different number.');
+            } else if (currentDifference > 50) {
                 $('#feedback').text('Very cold!');
-                userWinState = false;
-            } else if (difference <= 50 && difference > 30) {
+            } else if ( currentDifference <= 50 && currentDifference > 30) {
                 $('#feedback').text('Cold');
-                userWinState = false;
-            } else if (difference <= 30 && difference > 10) {
+            } else if (currentDifference <= 30 && currentDifference > 10) {
                 $('#feedback').text('Hot');
-                userWinState = false;
             } else {
                 $('#feedback').text('Very hot!');
-                userWinState = false;
             }
+            
         }
     }
     
 });
-
-/*
-still needs counter and difference btwn current & previous guess
-*/
